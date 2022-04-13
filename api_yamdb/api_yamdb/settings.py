53510a0e1a7f17dd66873d6a31443e894b26a3e8
Vadim Evlentev <vadim-evl@yandex.ru>
2022-04-13 10:47:45 +0300

    Набросок моделей Preview, Comments

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,12 +15,16 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Review(models.Model):
+    # author
+    # title
+    # text
+    # comment
+    # score
+    # created
+    pass
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
