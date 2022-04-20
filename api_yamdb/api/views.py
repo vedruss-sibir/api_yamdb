cd8a9f97e3a1b96315444d24b6b608d3f8416d32
Vadim Evlentev <vadim-evl@yandex.ru>
2022-04-20 11:36:26 +0300

    Убираем дубль

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -36,7 +36,7 @@ class UsersViewSet(viewsets.ModelViewSet):
     search_fields = ("username",)
 
     @action(
-        detail = False,
+        detail=False,
         methods=['GET', 'PATCH'],
         url_path='me',
         permission_classes=(IsAuthenticated,),
@@ -48,9 +48,13 @@ class UsersViewSet(viewsets.ModelViewSet):
             serializer = self.get_serializer(user)
             return Response(serializer.data, status=status.HTTP_200_OK)
         if request.method == "PATCH":
-            serializer = self.get_serializer(user, data=request.data, partial=True)
+            serializer = self.get_serializer(
+                user,
+                data=request.data,
+                partial=True
+            )
             serializer.is_valid(raise_exception=True)
-            serializer.save(role = user.role)
+            serializer.save(role=user.role)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
 
@@ -63,7 +67,7 @@ def create_user(request):
     real_username = User.objects.filter(username=username)
     real_email = User.objects.filter(email=email)
     if real_username.exists() or real_email.exists():
-        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
     user = User.objects.create_user(username=username, email=email)
     user.confirmation_code = uuid4()
     user.save()
@@ -93,50 +97,6 @@ def create_token(request):
         {'confirmation_code': 'Неверный код подтверждения'},
         status=status.HTTP_400_BAD_REQUEST
     )
-
-
-@api_view(["POST"])
-@permission_classes((AllowAny,))
-def create_user(request):
-    serializers = UserSerializer(data=request.data)
-    serializers.is_valid(raise_exception=True)
-    email = serializers.validated_data["email"]
-    username = serializers.validated_data["username"]
-    valid_user = User.objects.filter(email=email, username=username)
-    if valid_user.exists():
-        send_mail(
-            "Код для доступа к токену",
-            f"{valid_user[0].confirmation_code}",
-            DEFAULT_FROM_EMAIL,
-            [f"{email}"],
-        )
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    if not valid_user.exists():
-        confirmation_code = uuid4()
-        user, created = User.objects.get_or_create(
-            **serializers.validated_data, confirmation_code=confirmation_code
-        )
-        send_mail(
-            "Код для доступа к токену",
-            f"{user.confirmation_code}",
-            DEFAULT_FROM_EMAIL,
-            [f"{email}"],
-        )
-        return Response(serializers.data, status=status.HTTP_200_OK)
-
-
-@api_view(["POST"])
-@permission_classes((AllowAny,))
-def create_token(requset):
-    serializers = UserSerializer(data=requset.data)
-    serializers.is_valid(raise_exception=True)
-    username = serializers.validated_data["username"]
-    confirmation_code = serializers.validated_data["confirmation_code"]
-    user = get_object_or_404(User, username=username)
-    if confirmation_code != user.confirmation_code:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    token = RefreshToken.for_user(user)
-    return Response({"token": str(token.access_token)}, status=status.HTTP_200_OK)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
